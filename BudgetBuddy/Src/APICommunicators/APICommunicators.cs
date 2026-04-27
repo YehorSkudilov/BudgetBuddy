08b955f2c7b62a02537d8f0635837bb2003264df
Yehor <74692289+YehorSkudilov@users.noreply.github.com>
2026-04-27 15:05:18 -0700

    1

--- BudgetBuddy/Src/APICommunicators/APICommunicators.cs
+++ BudgetBuddy/Src/APICommunicators/APICommunicators.cs
@@ -4,7 +4,8 @@ public static class ApiCommunicators
 {
     //public const string BaseUrl = "http://10.0.2.2:5107";
     //public const string BaseUrl = "http://localhost:5107";
-    public const string BaseUrl = "http://192.168.1.68:5107";
+    //public const string BaseUrl = "http://192.168.1.68:5107";
+    public const string BaseUrl = "https://budgetbuddyapi.yehorskudilov.com";
 
 
 
